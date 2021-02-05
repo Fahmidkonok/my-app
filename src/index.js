6c5c30385ec1280c99d3b11da690fcbdfc2c7e82
Fahmidkonok <targetzone555@gmail.com>
2021-02-05 18:12:50 +0600

    edite
    1

--- src/index.js
+++ src/index.js
@@ -24,6 +24,7 @@ const square = function(number) {
 // const square = number => number * number;
 // console.log(square(5));
 
+/*
 const jobs = [
   { id: 1, isActive: true},
   { id: 2, isActive: true},
@@ -32,3 +33,74 @@ const jobs = [
 
 // const jobs.filter(function(job) { return job.isActive; });
 const  activeJobs = jobs.filter(job => job.isActive);
+
+*/
+
+
+// Arrow Function and this
+
+/*
+const person = {
+  talk() {
+    var self = this;
+    setTimeout(function() {
+      console.log("self", self);
+    },1000);
+  }
+};
+
+person.talk();
+
+*/
+
+//array : map
+
+/*
+const colors = ['red', 'greeen' ,'Blue']
+const items = colors.map(color => `<li>${color}</li>`);
+console.log(items);
+*/
+
+//Object Destructuring
+
+/*
+const address = {
+  street: '',
+  city: '',
+  country: '',
+};
+
+// const street = address.street;
+// const city = address.city;
+// const country = address.country;
+
+const {street: st } = address;
+
+*/
+
+// Spread operator
+
+/*
+const first = {name: "Mosh"};
+const second = {job: "instructor"};
+
+// const combined = { ...first, ...second, location: "Australia" };
+// console.log(combined);
+
+const clone = {...first};
+*/
+
+
+// Classes + Inheritance + modules
+
+//Named and Default Exports//
+
+
+import Teacher, { promote } from "./teacher";
+
+//Named -> import {.......} from '';
+//Default -> import .....from '';
+
+const teacher = new Teacher("Mosh", "Msc");
+
+teacher.teach();
